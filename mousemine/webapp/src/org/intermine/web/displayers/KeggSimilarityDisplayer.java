8f67bf3e1fe4f490a6e0e9efe322afb826c69959
Michael O'Keefe <okeefm@rpi.edu>
2011-10-17 14:46:55 -0400

    Updated KeggSimilarityDisplayer with some needed imports to get it to compile

--- mousemine/webapp/src/org/intermine/web/displayers/KeggSimilarityDisplayer.java
+++ mousemine/webapp/src/org/intermine/web/displayers/KeggSimilarityDisplayer.java
@@ -1,4 +1,4 @@
-package org.intermine.web.bio.displayer;
+package org.intermine.bio.web.displayer;
 
 /*
  * Copyright (C) 2002-2011 FlyMine
@@ -10,23 +10,51 @@ package org.intermine.web.bio.displayer;
  *
  */
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
 import org.intermine.api.InterMineAPI;
+import org.intermine.api.profile.Profile;
+import org.intermine.api.query.PathQueryExecutor;
+import org.intermine.api.results.ExportResultsIterator;
+import org.intermine.api.results.ResultElement;
+import org.intermine.metadata.Model;
+import org.intermine.model.InterMineObject;
+import org.intermine.model.bio.BioEntity;
+import org.intermine.model.bio.Organism;
+import org.intermine.pathquery.Constraints;
+import org.intermine.pathquery.OrderDirection;
+import org.intermine.pathquery.PathQuery;
+import org.intermine.web.displayer.ReportDisplayer;
+import org.intermine.web.logic.config.ReportDisplayerConfig;
+import org.intermine.web.logic.results.ReportObject;
+import org.intermine.web.logic.session.SessionMethods;
 
 /**
- * Displayer for similarity between genes based on KEGG pathways
+ * 
  * @author Michael O'Keefe
- *
  */
 public class KeggSimilarityDisplayer extends ReportDisplayer
 {
-    protected static final Logger LOG = Logger.getLogger(KeggSimilarityDisplayer.class);
 
-    public KeggSimilarityDisplayer(ReportDisplayerconfig config, InterMineAPI im) {
+    /**
+     * Construct with config and the InterMineAPI.
+     * @param config to describe the report displayer
+     * @param im the InterMine API
+     */
+    public KeggSimilarityDisplayer(ReportDisplayerConfig config, InterMineAPI im) {
         super(config, im);
     }
 
     @Override
     public void display(HttpServletRequest request, ReportObject reportObject) {
-
+    
+        return;
     }
 }
